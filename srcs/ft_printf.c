67bf88a70dc3041e5d2622be85f221cde3ee0f29
Alpha <alpha-shadows@protonmail.com>
2021-11-30 22:39:43 +0100

    Handle infinite args

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,19 +6,23 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 22:31:41 by jmaia             #+#    #+#             */
-/*   Updated: 2021/11/30 22:31:42 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/11/30 22:37:56 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdarg.h>
 
 int	ft_printf(const char *format, ...)
 {
+	va_list				ap;
 	unsigned int		i;
 	t_infinite_string	*output_line;
 
+	va_start(ap, format);
 	i = 0;
 	while (format[i])
-		treat_next_char(output_line, format, &i);
+		treat_next_char(output_line, format, &i, ap);
 	ft_putstr(get_str(output_line));
+	va_end(ap);
 }
