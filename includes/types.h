761ff2e50c8659c3eb970a2c6d3abe67ba312dc2
Jeremy Maia <jmaia@student.42.fr>
2021-12-02 19:41:28 +0100

    Replace data by precision

--- includes/types.h
+++ includes/types.h
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 19:28:47 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/02 19:30:47 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/02 19:37:02 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ typedef struct s_options
 {
 	unsigned char	flags;
 	int				minimal_field_width;
-	char			*data;
+	int				precision;
 	char			conv;
 }	t_options;
 #endif
