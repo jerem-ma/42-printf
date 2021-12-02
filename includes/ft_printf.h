8fc5cbf18a7bc05f9b1647bf88ec8bf673ba0775
Jeremy Maia <jmaia@student.42.fr>
2021-12-02 17:56:20 +0100

    Add error flag
    Added with value 1 so, every flag value is shifted

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,18 +6,20 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 18:22:32 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/02 15:48:59 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/02 17:00:54 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-# define FLAG_0_PAD 1
-# define FLAG_LEFT_ADJUSTED 2
-# define FLAG_PRECISION 4
-# define FLAG_ALTERNATE_FORM 8
-# define FLAG_BLANK 16
-# define FLAG_ALWAYS_SIGN 32
+
+# define FLAG_ERROR 1
+# define FLAG_0_PAD 2
+# define FLAG_LEFT_ADJUSTED 4
+# define FLAG_PRECISION 8
+# define FLAG_ALTERNATE_FORM 16
+# define FLAG_BLANK 32
+# define FLAG_ALWAYS_SIGN 64
 
 # include <unistd.h>
 # include <stdarg.h>
