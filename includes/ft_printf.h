7f634abb82f31e65cb5c07b75c3dfe8b67168c10
Jeremy Maia <jmaia@student.42.fr>
2021-12-02 15:34:36 +0100

    Add flag definition

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,12 +6,19 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 18:22:32 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/02 11:16:55 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/02 15:33:34 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# define FLAG_0_PAD 1
+# define FLAG_LEFT_ADJUSTED 2
+# define FLAG_PRECISION 4
+# define FLAG_ALTERNATE_FORM 8
+# define FLAG_BLANK 16
+# define FLAG_ALWAYS_SIGN 32
+
 # include <unistd.h>
 # include <stdarg.h>
 
