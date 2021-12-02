a3914eabea7a3d6a1064ad5db36bdc8cc5820ff9
Jeremy Maia <jmaia@student.42.fr>
2021-12-02 13:54:04 +0100

    Use as_str instead of get_str

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 22:31:41 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/01 15:34:35 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/02 13:50:08 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_printf(const char *format, ...)
 	i = 0;
 	while (format[i])
 		treat_next_char(&output_line_buffer, format, &i, ap);
-	output_line = get_str(&output_line_buffer);
+	output_line = as_str(&output_line_buffer);
 	ft_putstr_fd(output_line, 1);
 	va_end(ap);
 	if (err)
