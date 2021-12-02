0dbc483f54341967e66de93464a540ad13a78745
Jeremy Maia <jmaia@student.42.fr>
2021-12-02 11:17:37 +0100

    Give name format to ft_printf arg

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 18:22:32 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/01 15:20:17 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/02 11:16:55 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@
 # include "libft.h"
 # include "libdynamic_buffer.h"
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
 #endif
