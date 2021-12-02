1d53af1a5cd7ddfd66dab6e4049271ae917a440c
Jeremy Maia <jmaia@student.42.fr>
2021-12-02 15:54:16 +0100

    Move treat_char() to parse.c

--- srcs/parse.c
+++ srcs/parse.c
@@ -1,33 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/30 18:22:32 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/02 15:48:59 by jmaia            ###   ########.fr       */
+/*   Created: 2021/12/02 15:43:51 by jmaia             #+#    #+#             */
+/*   Updated: 2021/12/02 15:53:44 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-# define FLAG_0_PAD 1
-# define FLAG_LEFT_ADJUSTED 2
-# define FLAG_PRECISION 4
-# define FLAG_ALTERNATE_FORM 8
-# define FLAG_BLANK 16
-# define FLAG_ALWAYS_SIGN 32
+#include "ft_printf.h"
 
-# include <unistd.h>
-# include <stdarg.h>
-
-# include "libft.h"
-# include "libdynamic_buffer.h"
-
-int	ft_printf(const char *format, ...);
-int	treat_next_char(
+static int	treat_next_char(
 		t_dynamic_buffer *output_line_buffer, const char *format,
 		unsigned int *i, va_list ap
-		);
-#endif
+		)
+{
+	t_options	options;
+
+	if (format[*i++] != '%')
+		return (append(output_line_buffer, format[i - 1]));
+}
