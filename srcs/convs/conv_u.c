2b8fe019383833573ba4bffe20b7367d92ca6b72
Jeremy Maia <jmaia@student.42.fr>
2021-12-03 14:09:39 +0100

    u conv

---
+++ srcs/convs/conv_u.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conv_u.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 11:59:30 by jmaia             #+#    #+#             */
+/*   Updated: 2021/12/03 13:58:45 by jmaia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "conv_u.h"
+
+int	conv_u(t_dynamic_buffer *output_line_buffer, t_options *options,
+		unsigned int param)
+{
+	const unsigned char	*raw_data;
+
+	raw_data = ft_itoa(param);
+	if (!raw_data)
+		return (1);
+	return (apply_general_options(output_line_buffer, options, raw_data));
+}
