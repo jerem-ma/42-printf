e61d79ccd8c4c904ad02d921ca84137fa2bfb2f1
Jeremy Maia <jmaia@student.42.fr>
2021-12-03 11:52:21 +0100

    Add conv headers

---
+++ includes/convs.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   convs.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 11:50:47 by jmaia             #+#    #+#             */
+/*   Updated: 2021/12/03 11:51:59 by jmaia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONVS_H
+# define CONVS_H
+
+# include "conv_c.h"
+# include "conv_s.h"
+# include "conv_p.h"
+# include "conv_d.h"
+# include "conv_i.h"
+# include "conv_u.h"
+# include "conv_x.h"
+# include "conv_X.h"
+# include "conv_%.h"
+
+#endif
