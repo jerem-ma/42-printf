f8a1fae1be826fa5053fe8f949d3cf4bac31302a
Jeremy Maia <jmaia@student.42.fr>
2021-12-03 14:08:57 +0100

    oops wrong header name

--- srcs/convs/conv_d.c
+++ srcs/convs/conv_d.c
@@ -6,11 +6,11 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 11:59:30 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/03 13:57:55 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/03 14:04:33 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "conv_c.h"
+#include "conv_d.h"
 
 int	conv_d(t_dynamic_buffer *output_line_buffer, t_options *options, int param)
 {
