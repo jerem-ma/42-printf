5abaf2597223b62e882e8b102711ea545112fa92
Jeremy Maia <jmaia@student.42.fr>
2021-12-03 13:58:08 +0100

    D Conversion

---
+++ srcs/convs/conv_d.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conv_d.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 11:59:30 by jmaia             #+#    #+#             */
+/*   Updated: 2021/12/03 13:57:55 by jmaia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "conv_c.h"
+
+int	conv_d(t_dynamic_buffer *output_line_buffer, t_options *options, int param)
+{
+	const unsigned char	*raw_data;
+
+	raw_data = ft_itoa(param);
+	if (!raw_data)
+		return (1);
+	return (apply_general_options(output_line_buffer, options, raw_data));
+}
