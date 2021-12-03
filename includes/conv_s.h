874b706d2ff25278ae4ea7a81419798c13d155e4
Jeremy Maia <jmaia@student.42.fr>
2021-12-03 12:48:59 +0100

    Wow norminetize

--- includes/conv_s.h
+++ includes/conv_s.h
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 11:55:57 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/03 12:30:17 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/03 12:48:51 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include "types.h"
 # include "libdynamic_buffer.h"
 
-int	conv_s(t_dynamic_buffer *output_line, t_options *options, const char *param);
+int	conv_s(t_dynamic_buffer *output_line, t_options *options,
+		const char *param);
 #endif
