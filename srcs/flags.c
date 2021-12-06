6c37c39b4b8606d43384b224f8793d54f8a7a564
Jeremy Maia <jmaia@student.42.fr>
2021-12-06 12:29:22 +0100

    Create has_flag()

--- srcs/flags.c
+++ srcs/flags.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 18:59:54 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/03 11:25:54 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/06 12:28:10 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,9 @@ int	get_flag_code(char c)
 	if (c == '+')
 		return (FLAG_ALWAYS_SIGN);
 	return (FLAG_ERROR);
+}
+
+int	has_flag(int flags, int flag)
+{
+	return (flags & flag);
 }
