af6316f1c533aef204636cd332303928f57c0200
Jeremy Maia <jmaia@student.42.fr>
2021-12-06 14:29:09 +0100

    Create apply_number_precision()

--- includes/conv_general.h
+++ includes/conv_general.h
@@ -6,12 +6,14 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 12:19:23 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/06 12:36:49 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/06 14:27:44 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONV_GENERAL_H
 # define CONV_GENERAL_H
+
+# include <stdlib.h>
 
 # include "libdynamic_buffer.h"
 # include "libft.h"
@@ -21,4 +23,9 @@
 
 int	apply_general_options(t_dynamic_buffer *output_line_buffer,
 		t_options *options, const unsigned char *raw_data);
+
+int	apply_minimal_field_width(t_dynamic_buffer *output_line_buffer,
+		t_options *options, unsigned char *raw_data);
+
+int	apply_number_precision(t_options *options, unsigned char **raw_data);
 #endif
--- srcs/convs/conv_general.c
+++ srcs/convs/conv_general.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 14:25:14 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/06 12:37:20 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/06 14:28:28 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 //}
 
 int	apply_minimal_field_width(t_dynamic_buffer *output_line_buffer,
-		t_options *options, const unsigned char *raw_data)
+		t_options *options, unsigned char *raw_data)
 {
 	size_t	len_raw_data;
 	int		n_spaces;
@@ -31,10 +31,38 @@ int	apply_minimal_field_width(t_dynamic_buffer *output_line_buffer,
 	n_spaces = options->minimal_field_width - len_raw_data;
 	n_spaces *= (n_spaces > 0);
 	i = 0;
-	filler = (options->flags & FLAG_0_PAD) * '0'
-		+ !(options->flags & FLAG_0_PAD) * ' ';
+	filler = (!!has_flag(options->flags, FLAG_0_PAD)) * '0'
+		+ !(has_flag(options->flags, FLAG_0_PAD)) * ' ';
 	err = 0;
 	while (!err && i++ < n_spaces)
 		err = append(output_line_buffer, &filler);
 	return (err);
+}
+
+int	apply_number_precision(t_options *options, unsigned char **raw_data)
+{
+	int				n_digits;
+	int				n_zeros;
+	int				i;
+	unsigned char	*new_data;
+
+	n_digits = ft_strlen((char *) raw_data) - ((*raw_data)[0] == '-');
+	n_zeros = options->precision - n_digits;
+	n_zeros *= n_zeros > 0;
+	if (!n_zeros)
+		return (0);
+	new_data = malloc(sizeof(*new_data) * (ft_strlen((char *) *raw_data)
+				+ n_zeros + 1));
+	if (!new_data)
+		return (1);
+	ft_memcpy(new_data + (*(raw_data)[0] == '-') + n_zeros, *raw_data,
+		ft_strlen((char *) *raw_data) + 1);
+	i = 0;
+	while (i < n_zeros)
+		new_data[i++] = '0';
+	if (*(raw_data)[0] == '-')
+		new_data[i] = '-';
+	free(*raw_data);
+	*raw_data = new_data;
+	return (0);
 }
