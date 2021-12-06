8360a1ca839542b04a0442298121dcc116bad1df
Jeremy Maia <jmaia@student.42.fr>
2021-12-06 12:30:42 +0100

    Fix errors due to non import of flags

--- includes/conv_general.h
+++ includes/conv_general.h
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 12:19:23 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/03 12:21:08 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/06 12:20:52 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "libdynamic_buffer.h"
 # include "types.h"
+# include "flags.h"
 
 int	apply_general_options(t_dynamic_buffer *output_line_buffer,
 		t_options *options, const unsigned char *raw_data);
