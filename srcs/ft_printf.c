ddc30569580ed2d36dd0b0c2cdcc9f11db27b94a
Jeremy Maia <jmaia@student.42.fr>
2021-12-07 10:59:32 +0100

    Fix char code 0 not prited

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 22:31:41 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/03 11:25:14 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/07 10:52:55 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	ft_printf(const char *format, ...)
 	while (format[i])
 		err += treat_next_char(&output_line_buffer, format, &i, ap);
 	output_line = as_str(&output_line_buffer);
-	ft_putstr_fd(output_line, 1);
+	ft_put_bytes_fd(output_line, output_line_buffer.i, 1);
 	va_end(ap);
 	if (err)
 		return (-1);
