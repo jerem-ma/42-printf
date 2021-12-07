8baf6301af1749f52f5522fc2710d38632b3e81d
Jeremy Maia <jmaia@student.42.fr>
2021-12-07 16:46:27 +0100

    Oops forgot files

---
+++ includes/annoying_0_flag.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   annoying_0_flag.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 15:38:11 by jmaia             #+#    #+#             */
+/*   Updated: 2021/12/07 15:39:37 by jmaia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANNOYING_0_FLAG_H
+# define ANNOYING_0_FLAG_H
+
+# include "flags.h"
+# include "conv_general.h"
+# include "types.h"
+
+int	annoying_0_flag(t_options *options, unsigned char **raw_data);
+#endif
